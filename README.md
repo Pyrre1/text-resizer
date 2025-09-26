2fb68f7d884144ebbfd6a0508c0ed3d4e241e902
Emanuel U <eu222ei@student.lnu.se>
2025-09-26 13:58:27 +0200

    Add some validation and standardization for future improvements

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # text-resizer
 
-A lightweight JavaScript module to resize text dynamically. Useful for accessibility, user customization, or responsive design.
+## General information
+
+## Purpose
+A JavaScript module designed to make text more accessible.
+This module allows developers to target different text elements and provide end users with options such as:
+
+- Changing the text size
+
+- Increasing contrast between text and background
+
+- Switching to a more readable font
+
+It is especially useful for users with dyslexia, color blindness, or other visual impairments.
 
 ## Installation
 
--- src/controller.js
+++ src/controller.js
@@ -10,11 +10,25 @@ export function createTextResizerController(config) {
     root = document
   } = config
 
+  const parsedStep = validateStep(step)
+  
+    /**
+   * Get all target elements based on provided selectors within the specified root.
+   * 
+   * @constant {Element[]} elements - Array of target elements to be resized.
+   */
   const elements = getTargetElements(selectors, root)
 
-  /**
-   * Loops through all elements to store the choosen unit format, and original size.
+    /**
+   * Stores the original size and units for each target element.
+   * This enables unit-aware resizing without forcing convertion to one unit.
    * 
+   * example stored values:
+   * - element: <p>, originalTextSize: "1.2em", unit: "em"
+   * - element: <h1>, originalTextSize: "18px", unit: "px"
+   * 
+   * @constant {Map<Element, string>} originalTextSizes - Map of elements to their original font sizes.
+   * @constant {Map<Element, string>} originalUnits - Map of elements to their original units.
    */
   const originalTextSizes = new Map()
   const originalUnits = new Map()
@@ -28,7 +42,24 @@ export function createTextResizerController(config) {
     }
   })
 
-  const parsedStep = validateStep(step)
+  // Check the unit for step value.
+  const stepUnit = checkStepUnit(step)
+
+  // Check all selected elements for unit consistency.
+  const unit = checkUnitConsistency(originalUnits)
+
+  // If there is no consistant unit, convert all to pixels.
+  if (unit === null) {
+    convertAllToPixels()
+    unit = 'px'
+  } 
+
+  // Check if step unit is same as element unit.
+  if (unit != stepUnit) {
+    // If the step unit is different from the elements unit, convert step to element unit.
+    convertStepToUnit(stepUnit, unit)
+  } // else units are the same, no conversion needed.
+
 
   // The increase text size by one step function.
   function increase() {
@@ -37,7 +68,7 @@ export function createTextResizerController(config) {
       const newSize = computeAdjustedSize(currentSize, parsedStep)
 
       const clampedSize = Math.min(newSize, maxSize)
-      element.style.fontSize= `${clampedSize}px`
+      element.style.fontSize= `${clampedSize}${unit}`
     })
   }
 
@@ -48,28 +79,28 @@ export function createTextResizerController(config) {
       const newSize = computeAdjustedSize(currentSize, -parsedStep)
 
       const clampedSize = Math.max(newSize, minSize)
-      element.style.fontSize = `${clampedSize}px`
+      element.style.fontSize = `${clampedSize}${unit}`
     })
   }
 
   // The instant max size function.
   function setTextToMax() {
     elements.forEach(element => {
-      element.style.fontSize = `${maxSize}px`
+      element.style.fontSize = `${maxSize}${unit}`
     })
   }
 
   // The instant min size function.
   function setTextToMin() {
     elements.forEach(element => {
-      element.style.fontSize = `${minSize}px`
+      element.style.fontSize = `${minSize}${unit}`
     })
   }
 
   // The restore to original size function.
   function restore() {
     elements.forEach(element => {
-      const originalSize = originalSizes.get(element)
+      const originalSize = originalTextSizes.get(element)
       if (originalSize) {
         element.style.fontSize = originalSize
       }
@@ -84,7 +115,31 @@ export function createTextResizerController(config) {
     restore
   }
 
+  /**
+   * Checks that all elements share the same font-size unit.
+   * 
+   * @param {Map<Element, string>} originalUnits - Map of elements to their units.
+   * @return {boolean} - Returns the consistant unit, otherwise null.
+   */
+  function checkUnitConsistency(originalUnits) {
+    const units = new Set()
+    originalUnits.forEach(unit => {
+      units.add(unit)
+    })
+    return units.size === 1 ? [...units][0] : null
+  }
 
+  // Check the unit of the step value, if no value is given, default to 'px'.
+  function checkStepUnit(step) {
+
+    if (typeof step === 'number') return 'px'
+
+    const match = step.match(/^(\d+)([a-z%]+)$/)
+    if (match) return match[2]
+    return 'px' // Default to 'px' if no unit found.
+  }
+
+  
   // Parsing of step to be able to use it as a integer regardless of if 'px' is added.
   function validateStep(step) {
     if (typeof step === 'number') return step
